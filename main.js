bac1537bdf67fe99df47b45ddf85f33837655e5c
Sean Manhertz <sean.manhertz@gmail.com>
2014-04-07 11:51:27 -0600

    Added name prompt,need to figure out recursive loop

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log ("Hello Boulder!")
+var name=prompt ('Enter your name!', 'name');
+if (name.length === 0){
+	alert('Please enter a name');
+	name=prompt ('Enter your name!', 'name');
+}
+else{
+	alert('Hello '+name+'!')
+}
